f21b98cd1b8211b992b58187ca871b986d4c43fd
Cristiano Castilho <126888502+CrCastilho94@users.noreply.github.com>
2023-04-05 11:29:01 -0400

    Add files via upload

---
+++ Scrypts - Cristiano.py
@@ -0,0 +1,25 @@
+nome = input ('Qual é o seu nome')
+print ('Salve', nome, 'Seja bem vindo')
+
+dia = input('qual e o dia do seu nascimento')
+mes = input ('qual o mes do seu nascimento')
+ano = input (' qual o ano do seu nascimento')
+print ("você nasceu em:",dia+"/"+mes+"/"+ano)
+
+ 
+n1 = input ("digite um numero")
+n2 = input ("digite um numero")
+soma = int (n1) + int (n2)
+print (" A soma entre eles é", soma)
+
+
+
+
+
+
+
+
+ 
+
+ 
+ 
