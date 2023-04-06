eb20b84e735973c1378e4ababf59d1f656feef56
Cristiano Castilho <126888502+CrCastilho94@users.noreply.github.com>
2023-04-06 11:21:05 -0400

    Add files via upload

---
+++ ex001.py
@@ -0,0 +1,5 @@
+print("olá, Mundo")
+msg ='olá mundo'
+print(msg)
+
+
---
+++ ex005.py
@@ -0,0 +1,8 @@
+a= input('Digite algo')
+print ('O tipo primitivo desse valor é', type (a))
+print ('Só tem espacos', a.isspace())
+print('È um número', a.isnumeric())
+print('È alfabético', a.isalpha())
+print('È alfanumero', a.isalnum())
+print('Esta em maiusculas', a.isupper())
+print('Esta em minusculas', a.islower())
